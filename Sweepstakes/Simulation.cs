4f2011d3473855dc0fd37d08df8583c9119d176d
Rob Clark <clarkrob13@gmail.com>
2021-02-09 11:28:02 -0500

    CreateMarketingFirmWithManager method complete with factory design to choose between Queue and Stack Manager

--- Sweepstakes/Simulation.cs
+++ Sweepstakes/Simulation.cs
@@ -10,7 +10,20 @@ namespace Sweepstakes
     {
         public void CreateMarketingFirmWithManager()
         {
-            string UserInterface.GetUserInputFor("Would you like to use a Queue Manager or Stack Manager for your sweepstakes? Press 1 for Queue or Press 2 for Stack");
+            string type = UserInterface.GetUserInputFor("Would you like to use a Queue Manager or Stack Manager for your sweepstakes? Press 1 for Queue or Press 2 for Stack");
+            MarketingFirm marketingFirm = new MarketingFirm(ManagerChoice(type));
+        }
+        public ISweepstakesManager ManagerChoice(string type)
+        {
+            switch (type)
+            {
+                case "1":
+                    return new SweepstakesQueueManager();
+                case "2":
+                    return new SweepstakesStackManager();
+                default:
+                    throw new ApplicationException(string.Format("Not a valid choice"));
+            }
         }
 
     }
